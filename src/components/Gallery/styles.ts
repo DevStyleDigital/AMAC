8a119eb0705c1b99d17d08a49a728de290024966
AfonsoBezerra <afonsobezerra199@gmail.com>
2022-05-22 19:53:23 -0300

    GalleryFinish

--- src/components/Gallery/styles.ts
+++ src/components/Gallery/styles.ts
@@ -18,8 +18,8 @@ export const Container = styled.div`
     .sombraImg{
       position:absolute;
       width:100%;
-      height: 100%;
-      top:0;
+      height: 50%;
+      bottom:0;
       left:0;
       z-index: 8;
       mix-blend-mode: multiply;
